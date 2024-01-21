d4ea9c416ac606e1f2e66eae7c0207cc7e02bd3f
daniyalkhan131 <daniyalkhan131@gmail.com>
2024-01-21 19:00:22 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='creating a ML project for r predicting taxi fare',
+    author='Daniyal Khan',
+    license='',
+)
