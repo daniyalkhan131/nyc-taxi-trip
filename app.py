37f14e14c7aedab3e7e935357c15f324928383f4
daniyalkhan131 <daniyalkhan131@gmail.com>
2024-01-26 20:53:24 +0530

    changed train_models.py as added mlflow for trackng hyperparameter tuning

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 from fastapi import FastAPI
 from joblib import load
 from pydantic import BaseModel
+import pandas as pd
+
+
+from src.features.fearure_definations import feature_build
 
 app = FastAPI()
 
@@ -10,20 +14,13 @@ class PredictionInput(BaseModel): #we create class for input passing and inherit
     # Define the input parameters required for making predictions
 
     vendor_id: float
+    pickup_datetime: float
     passenger_count: float
     pickup_longitude: float
     pickup_latitude: float
     dropoff_longitude: float
     dropoff_latitude: float
     store_and_fwd_flag: float
-    distance_haversine: float
-    distance_dummy_manhattan: float
-    direction: float
-    pickup_weekday: float
-    pickup_hour: float
-    pickup_minute: float
-    pickup_dt: float
-    pickup_week_hour: float
 
 # Load the pre-trained RandomForest model
 model_path = "models/model.joblib"
@@ -38,25 +35,26 @@ def predict(input_data: PredictionInput): #here we give the class as input
     #so that input should be taken care by pydantic before going into model
 
     # Extract features from input_data and make predictions using the loaded model
-    features = {input_data.vendor_id,
-                input_data.passenger_count,
-                input_data.pickup_longitude,
-                input_data.pickup_latitude,
-                input_data.dropoff_longitude,
-                input_data.dropoff_latitude,
-                input_data.store_and_fwd_flag,
-                input_data.distance_haversine,
-                input_data.distance_dummy_manhattan,
-                input_data.direction,
-                input_data.pickup_weekday,
-                input_data.pickup_hour,
-                input_data.pickup_minute,
-                input_data.pickup_dt,
-                input_data.pickup_week_hour
-            }
+    features = {
+            'vendor_id': input_data.vendor_id,
+            'pickup_datetime': input_data.pickup_datetime,
+            'passenger_count': input_data.passenger_count,
+            'pickup_longitude': input_data.pickup_longitude,
+            'pickup_latitude': input_data.pickup_latitude,
+            'dropoff_longitude': input_data.dropoff_longitude,
+            'dropoff_latitude': input_data.dropoff_latitude,
+            'store_and_fwd_flag': input_data.store_and_fwd_flag
+}
     
-    print(model.predict([features])[0].item())
-    prediction = model.predict([features])[0].item()
+    print("features are", features)
+    
+
+    features = pd.DataFrame(features, index=[0])
+    features = feature_build(features, 'prod')
+
+    print('PREDICTION',model.predict(features.values)[0].item())
+    
+    prediction = model.predict(features.values)[0].item()
     #[0] becuase it returns numpy int but with web framework it is not compatible so
     #we have to give things that is directly understand by python
     # Return the prediction
@@ -64,4 +62,4 @@ def predict(input_data: PredictionInput): #here we give the class as input
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
